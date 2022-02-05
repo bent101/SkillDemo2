0e15e9c9e97c8abfa31469dc15a840880617e36d
Ben Tomlin <57016008+bent101@users.noreply.github.com>
2022-02-04 16:46:41 -0800

    make a failing test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void testSkillDemo() {
+        assertEquals(11, SkillDemo.multiply(2, 5));
+    }
+}
